a44ec960d0b0fc8835550005578339add6e0f6e8
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-11-02 00:12:06 +0500

    Declaring variabes and getting values

--- app/src/main/java/com/example/kidsgame/MainActivity.java
+++ app/src/main/java/com/example/kidsgame/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.kidsgame;
+import java.lang.Math;
+import java.util.Scanner;
+import java.util.Random;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1, textView2, textView3;
+
+    Button button, button2, button3, button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+
+
+
     }
 }
